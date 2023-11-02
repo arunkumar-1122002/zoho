e1dd6b8e95ea43cc079a5c65b6195a7f5ab5a4ac
arunkumar-1122002 <148625880+arunkumar-1122002@users.noreply.github.com>
2023-11-02 14:03:26 +0530

    Update Print char by count.java

--- Print char by count.java
+++ Print char by count.java
@@ -10,7 +10,7 @@ NUMBER RANGE FROM 1 TO 99
 */
 public class Printcharbycount{
   public static void main(String[] args){
-    String s="B3C6D11";
+    String s="B3";
     char cursor=s.charAt(0);
     int i=0,j,num=0,next;
     do{
@@ -21,12 +21,19 @@ public class Printcharbycount{
         cursor=s.charAt(i);
         j=i+1;
         next=i+2;
+        try{
         if(s.charAt(next)<58 && s.charAt(next)>47){
           num=s.charAt(next)-48;
           num =((s.charAt(j)-48)*10)+num;
           i+=3;
         }
         else{
+          num=s.charAt(j)-48;
+          i+=2;
+      }}
+      catch(Exception e)
+      {
+        
           num=s.charAt(j)-48;
           i+=2;
       }
