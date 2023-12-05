ba6123a5bde2eee8a8b3bd18d3eec3f3fa7752e2
ARUN KUMAR N <148625880+arunkumar-1122002@users.noreply.github.com>
2023-12-05 21:05:48 +0530

    Update pyramid_pattern.java

--- pyramid_pattern.java
+++ pyramid_pattern.java
@@ -1,29 +1,34 @@
-package helloworld;
+/*
+input:
+n=5
+output:
+        1 
+      2 3 2 
+    3 4 5 4 3 
+  4 5 6 7 6 5 4 
+5 6 7 8 9 8 7 6 5
+*/
 
-public class Ak
-{
+public class pyramid_pattern {
+
     public static void main(String[] args) {
-   int n=5;
-   for(int i=0;i<n;i++)
-   {
-       for(int j=i;j<n-1;j++)
-       {
-           System.out.print("  ");
-       }
-       int temp=i+1;
-       for(int j=n-i;j<=n;j++)
-       {
-           System.out.print(temp+" ");
-           temp++;
-       }
-       temp=((i+1)*2)-2;
-       int z=i;
-       while(z>0)
-       {System.out.print(temp+" ");
-       temp--;
-           z--;
-       }
-       System.out.println("");
-   }
+        int n=7;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int k = 1; k <=n-i; k++) {
+                System.out.print("  ");   
+            }
+            int l=i-1;
+            for (int j = 1; j <=(i+i)-1; j++) {
+                if(j<=i)
+                    System.out.print((++l)+" ");
+                else
+                    System.out.print((--l)+" ");
+            }
+            System.out.println();
+        }
+        
+    }
+    
 }
-}
+
